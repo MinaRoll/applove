74be734761612ce9d980250f05002dc66f0e91d1
romina <romina.torres.b@gmail.com>
2017-06-01 10:17:03 -0400

    segunda parte ejercicio evaluado

--- js/app.js
+++ js/app.js
@@ -2,17 +2,28 @@
  * Archivo principal de funcionalidad de JS
  */
  (function main(){
- 	var boxes = Array.from(document.getElementsClassName("box-services"));
- 	var modal = document.getElemetnById("box-services-modal");
- 	var bodyModal;
- 	boxes.forEach(function(box)){
+ 	var boxes = Array.from(document.getElementsByClassName('box-work'));
+ 	var modal = document.getElementById("box-work-modal");
+ 	var bodyModal, close, img;
+ 	boxes.forEach(function(box, i){
  		box.addEventListener("click", function(){
- 			modal.classList.remove("hide")
- 		})
-
- 		bodyModal = document.createElement("div");
- 		bodyModal.classList.add("modal-body");
- 		bodyModal.innerHtml = box.innerHTML;
-
- 	}
- }
+ 			modal.innerHTML = "";
+ 			bodyModal = document.createElement("div");
+ 			bodyModal.classList.add("modal-body");
+ 			bodyModal.innerHTML = box.innerHTML;
+      //remueve el texto del div para mostrar solo img.
+      bodyModal.removeChild(bodyModal.childNodes[3]);
+ 			modal.appendChild(bodyModal);
+ 			modal.classList.remove("hide");
+ 			close = document.createElement("div");
+ 			close.classList.add("close");
+ 			img = document.createElement("img");
+ 			img.setAttribute("src", "assets/images/close1.png");
+ 			close.appendChild(img);
+ 			modal.appendChild(close);
+ 			close.addEventListener("click",function(){
+ 				modal.classList.add("hide");
+ 			});
+ 		});
+ 	});
+ })();
